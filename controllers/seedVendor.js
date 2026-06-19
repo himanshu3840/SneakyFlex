c4473ca6f51fa3d1b4934b99d36baada5f202a66
Himanshu Kumar <himanshuecekumar@gmail.com>
2026-06-20 00:45:32 +0530

    ho

--- controllers/seedVendor.js
+++ controllers/seedVendor.js
@@ -1,24 +1,33 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const vendorModel = require("../models/vendor-model");
 
-mongoose.connect(process.env.MONGODB_URI);
-
 async function createVendorKeys() {
-    await vendorModel.create([
-        { key: "VENDOR2026ABC" },
-        { key: "VENDOR2026DEF" },
-        { key: "VENDOR2026GHI" },
-        { key: "VENDOR2026JKL" },
-        { key: "VENDOR2026MNO" },
-        { key: "VENDOR2026PQR" },
-        { key: "VENDOR2026STU" },
-        { key: "VENDOR2026VWX" },
-        { key: "VENDOR2026YZA" },
-        { key: "VENDOR2026BCD" }
-    ]);
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("10 vendor keys inserted");
-    mongoose.connection.close();
+        await vendorModel.deleteMany({});
+
+        await vendorModel.create([
+            { key: "VENDOR2026ABC" },
+            { key: "VENDOR2026DEF" },
+            { key: "VENDOR2026GHI" },
+            { key: "VENDOR2026JKL" },
+            { key: "VENDOR2026MNO" },
+            { key: "VENDOR2026PQR" },
+            { key: "VENDOR2026STU" },
+            { key: "VENDOR2026VWX" },
+            { key: "VENDOR2026YZA" },
+            { key: "VENDOR2026BCD" }
+        ]);
+
+        console.log("10 vendor keys inserted");
+        process.exit();
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
 }
 
 createVendorKeys();
